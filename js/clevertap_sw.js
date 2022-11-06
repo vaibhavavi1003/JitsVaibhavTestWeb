0f75e9933e524c2b48606282eebcbe62a4ecba62
Jitendra Sahoo <80240215+JitS-Clever@users.noreply.github.com>
2022-11-06 21:49:50 +0530

    Update clevertap_sw.js

--- js/clevertap_sw.js
+++ js/clevertap_sw.js
@@ -1,1 +1,3 @@
-importScripts('https://s3-eu-west-1.amazonaws.com/static.wizrocket.com/js/sw_webpush.js');
+ if( 'undefined' === typeof window){
+  importScripts('https://s3-eu-west-1.amazonaws.com/static.wizrocket.com/js/sw_webpush.js');
+}
