f94cb096d9ff40ba0ce9319c1922759e29bd67fa
Tuaha Khan <tuaha.work@gmail.com>
2024-07-05 20:55:20 +0530

    Disappearing InApp template getting generated

--- js/inapp.js
+++ js/inapp.js
@@ -1,8 +1,8 @@
 var templateJson;
 var templateCode;
-
+var selectedTemplateName;
+var formValues={};
 function startLoader() {
-
     var bodyTag = document.querySelector('body');
     bodyTag.classList.add('blurbg');
     var loaderDiv = document.createElement("div");
@@ -15,29 +15,12 @@ document.getElementById("templateType").querySelectorAll('.dropdown-item').forEa
             
     el.addEventListener('click', function() {
         document.querySelector('.dropdown-toggle').innerText = el.textContent;
-        templateJson = {
-            "0": {
-                "label": "Please Enter Timer Duration :",
-                "type": "Text",
-                "id": "timer"
-            },
-            "1": {
-                "label": "Enter Background Imager Url :",
-                "type": "Image",
-                "id": "imgUrl",
-                "description":"image size 300*300"
-            },
-            "2": {
-                "label": "Button Redirection",
-                "type": "Text",
-                "id": "deeplink"
-            },
-            "max":2
-        }
-        var templateCode = "<!DOCTYPE html>\n<html lang=\"en\">\n\n<head>\n    <meta charset=\"UTF-8\">\n    <title>video ad</title>\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n    <link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">\n    <link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>\n    <link href=\"https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap\" rel=\"stylesheet\">\n    <script>\n        var redirectURL = \"https://jits-clever.github.io/TestWeb/\";\n        var videoURL = \"https://shopifyctjt.s3.ap-south-1.amazonaws.com/sale.gif\";\n    </script>\n    <style>\n\n        .video-container {\n            position: relative;\n            overflow: hidden;\n            border-radius: 1.5rem;\n            margin: 1rem auto;\n            transition: all 2s ease; \n        }\n\n        .video-container img {\n            width: 100%;\n            object-fit: contain;\n            height: 250px;\n            width: 150px;\n        }\n\n        .dismiss {\n            position: absolute;\n            top: 1rem;\n            left: 1rem;\n            color: white;\n            font-size: 2rem;\n            cursor: pointer;\n        }\n\n        .small-popup {\n            width: auto; /* Adjust size as needed */\n            height: auto;\n            bottom: 10px; /* Adjust position as needed */\n            right: 10px; /* Adjust position as needed */\n            border-radius: 0.5rem;\n            position: fixed;\n            animation: slideIn 2s forwards;\n        }\n\n        @keyframes slideIn {\n            from {\n                transform: translate(100%, 100%);\n            }\n            to {\n                transform: translate(0, 0);\n            }\n        }\n\n    </style>\n</head>\n\n<body>\n    <div class=\"small-popup\" id=\"popup\">\n        \n        <div class=\"video-container\" id=\"videoContainer\">\n            <span class=\"dismiss\" onclick=\"closePopup()\">✕</span>\n        </div>\n    </div>\n\n    <script>\n        var video = document.getElementById('videoContainer');\n        var source = document.createElement('img');\n\n        source.setAttribute('src', videoURL);\n        source.setAttribute('id', 'myVideo');\n\n        video.appendChild(source);\n        document.addEventListener(\"DOMContentLoaded\", function () {\n\n            const videoTag = document.getElementById('myVideo');\n            videoTag.addEventListener(\"click\",function (){\n                window.location.href = redirectURL;\n            })\n        });\n        function closePopup() {\n            popup.style.display = \"none\"; // Hide the popup\n        }\n    </script>\n</body>\n\n</html>"
-        
+        selectedTemplateName='Disappearing Inapp'
         // startLoader()
-        builder()
+        fetchData(selectedTemplateName)
+        // var tempcode=fetchData.templateJson;
+        // console.log(templateJson)
+        // builder()
     });
 });
 
@@ -56,14 +39,20 @@ function templateChange(templateName,templateJson) {
     }
 }
 
-function builder(){
-    const form=document.getElementById('dynamicContent')
-    form.innerHTML='';
+function formBuilder() {
+    const form = document.getElementById('dynamicContent');
+    form.innerHTML = '';
+    var maxFeilds = templateJson.json_content.max;
+    var fields = templateJson.json_content;
 
-    for(let i=0; i<=templateJson.max;i++){
-        const field=templateJson[i];
+
+    console.log(templateJson.html_content)
+
+    for (let i = 0; i <= maxFeilds; i++) {
+        const field = fields[i];
         const formGroup = document.createElement('div');
         formGroup.className = 'form-group mb-3';
+        console.log(field.description);
 
         const label = document.createElement('label');
         label.setAttribute('for', field.id);
@@ -71,14 +60,13 @@ function builder(){
         formGroup.appendChild(label);
 
         let input;
-        if(field.type=='Text'){
-            input=document.createElement('input');
+        if (field.type == 'Text') {
+            input = document.createElement('input');
             input.className = 'form-control';
             input.setAttribute('id', field.id);
             formGroup.appendChild(input);
-            console.log("Runing in txt")
-        }else if(field.type=='Image'){
-            input=document.createElement('input'); 
+        } else if (field.type == 'Image') {
+            input = document.createElement('input');
             input.className = 'form-control';
             input.setAttribute('id', field.id);
             formGroup.appendChild(input);
@@ -88,15 +76,52 @@ function builder(){
             description.textContent = field.description;
             formGroup.appendChild(description);
         }
-        // else if(field.type=='deeplink'){
-        //     input=document.createElement('input'); 
-        // }
-       
 
         form.appendChild(formGroup);
-
     }
 }
+function fetchFormData() {
+    const form = document.getElementById('dynamicContent');
+    const inputs = form.querySelectorAll('input');
+
+    inputs.forEach(input => {
+        formValues[input.id] = input.value;
+    });
+
+    console.log(formValues)
+    codeBuilder()
+}
+
+
+async function fetchData(templateName){
+
+    const url = `https://v5ffl5exja.execute-api.ap-south-1.amazonaws.com/prod?inappTemplate=${templateName}`;
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            mode: 'cors'  // Explicitly set CORS mode
+
+        });
+
+        if (!response.ok) {
+            throw new Error('Error in response');
+        }
+
+        templateJson = await response.json();
+        templateCode=templateJson.html_content
+        formBuilder();
+    } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+    }
+    
+}
+
+
+
+
 
 function copyCode() {
     const codeBlock = document.getElementById('codeBlock');
@@ -116,4 +141,31 @@ function loadIframe() {
     iframeDoc.open();
     iframeDoc.write(codeBlock);
     iframeDoc.close();
+}
+
+
+function codeBuilder(){
+    if(selectedTemplateName=='Disappearing Inapp'){
+        var imageUrl=formValues.imgUrl;
+        var time=formValues.timer;
+        var deepLink=formValues.deeplink;
+        
+
+        templateCode = templateCode.replace(/https:\/\/shopifyctjt.s3.ap-south-1.amazonaws.com\/bmw.jpg/g, imageUrl);
+        templateCode = templateCode.replace(/https:\/\/www.jitdemo.jitendract.com\/webview/g, deepLink);
+
+        templateCode = templateCode.replace(/animation: progress \d+s linear forwards;/g, `animation: progress ${time}s linear forwards;`);
+
+
+        const codeBlock = document.getElementById('codeBlock');
+        codeBlock.value=templateCode;
+    navigator.clipboard.writeText(templateCode)
+    .then(() => {
+        console.log('Code copied to clipboard!');
+    })
+    .catch(err => {
+        console.error('Failed to copy: ', err);
+    });
+    }
+    
 }
